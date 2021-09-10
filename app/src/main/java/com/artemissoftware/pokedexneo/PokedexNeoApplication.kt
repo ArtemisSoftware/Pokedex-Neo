9301fd3c547cb388f6362b439821ca2eb6d7b8c1
gustavo <gustavo.isaac@gmail.com>
2021-09-10 17:28:46 +0100

    PokedexNeoApplication

--- app/src/main/java/com/artemissoftware/pokedexneo/PokedexNeoApplication.kt
+++ app/src/main/java/com/artemissoftware/pokedexneo/PokedexNeoApplication.kt
@@ -2,12 +2,13 @@ package com.artemissoftware.pokedexneo
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class PokedexNeoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        //--Timber.plant(Timber.DebugTree())
+        Timber.plant(Timber.DebugTree())
     }
 }
