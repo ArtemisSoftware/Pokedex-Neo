a52cd328d060b65f50e4fbe6de977167be6e7a70
gustavo <gustavo.isaac@gmail.com>
2021-09-11 17:35:51 +0100

    PokemonListScreen Part 1 - Searchbar

--- app/src/main/java/com/artemissoftware/pokedexneo/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/pokedexneo/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.artemissoftware.pokedexneo.pokemonlist.PokemonListScreen
 import com.artemissoftware.pokedexneo.ui.theme.PokedexNeoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,8 +34,7 @@ class MainActivity : ComponentActivity() {
                 ) {
 
                     composable("pokemon_list_screen"){
-
-
+                        PokemonListScreen(navController = navController)
                     }
 
                     composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
