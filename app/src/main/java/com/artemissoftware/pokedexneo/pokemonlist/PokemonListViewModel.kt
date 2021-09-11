ebcbe745f63d6879f9986056a55d43c8f70993d1
gustavo <gustavo.isaac@gmail.com>
2021-09-11 17:45:17 +0100

    PokemonListViewModel

--- app/src/main/java/com/artemissoftware/pokedexneo/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/artemissoftware/pokedexneo/pokemonlist/PokemonListViewModel.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.palette.graphics.Palette
 import com.artemissoftware.pokedexneo.repository.PokemonRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -13,14 +14,14 @@ import javax.inject.Inject
 class PokemonListViewModel @Inject constructor(private val repository: PokemonRepository) : ViewModel() {
 
 
-//    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
-//        val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
-//
-//        Palette.from(bmp).generate { palette ->
-//            palette?.dominantSwatch?.rgb?.let { colorValue ->
-//                onFinish(Color(colorValue))
-//            }
-//        }
-//    }
+    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
+        val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        Palette.from(bmp).generate { palette ->
+            palette?.dominantSwatch?.rgb?.let { colorValue ->
+                onFinish(Color(colorValue))
+            }
+        }
+    }
 
 }
