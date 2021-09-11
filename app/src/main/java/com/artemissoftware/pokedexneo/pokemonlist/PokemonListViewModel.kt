a9586dfd5b26fe24296b330738a4309ce1925f49
gustavo <gustavo.isaac@gmail.com>
2021-09-11 18:09:28 +0100

    Method to get pokemon pages

--- app/src/main/java/com/artemissoftware/pokedexneo/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/artemissoftware/pokedexneo/pokemonlist/PokemonListViewModel.kt
@@ -3,15 +3,73 @@ package com.artemissoftware.pokedexneo.pokemonlist
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
+import com.artemissoftware.pokedexneo.api.PokeApi.Companion.IMAGE_URL
+import com.artemissoftware.pokedexneo.api.PokeApi.Companion.PAGE_SIZE
+import com.artemissoftware.pokedexneo.data.models.PokedexListEntry
 import com.artemissoftware.pokedexneo.repository.PokemonRepository
+import com.artemissoftware.pokedexneo.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(private val repository: PokemonRepository) : ViewModel() {
+
+
+    private var curPage = 0
+
+    var pokemonList = mutableStateOf<List<PokedexListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+
+    fun loadPokemonPaginated() {
+
+        viewModelScope.launch {
+
+            isLoading.value = true
+
+            val result = repository.getPokemonList(PAGE_SIZE, curPage * PAGE_SIZE)
+
+            when(result) {
+
+                is Resource.Success -> {
+
+                    endReached.value = curPage * PAGE_SIZE >= result.data!!.count
+
+                    val pokedexEntries = result.data.results.mapIndexed { index, entry ->
+
+                        val number = if(entry.url.endsWith("/")) {
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        } else {
+                            entry.url.takeLastWhile { it.isDigit() }
+                        }
+
+                        val url = "$IMAGE_URL${number}.png"
+                        PokedexListEntry(entry.name.capitalize(Locale.ROOT), url, number.toInt())
+                    }
+                    curPage++
+
+                    loadError.value = ""
+                    isLoading.value = false
+                    pokemonList.value += pokedexEntries
+                }
+
+                is Resource.Error -> {
+                    loadError.value = result.message!!
+                    isLoading.value = false
+                }
+            }
+        }
+    }
+
 
 
     fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
